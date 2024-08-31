4f2d7a292844a0440e340cec67050d89734338b1
l1ancg <chuanguang.lian@gmail.com>
2024-08-31 16:25:53 +0800

    add vercel/analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,19 @@
 import { AppProps } from "next/app";
 import "@/styles/globals.css";
 import Header from "@/components/Header";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <div className="h-screen flex flex-col">
       <Header />
-      <div className="h-full layout flex-1" style={{ height: `calc(100vh - 150px)` }}>
+      <div
+        className="h-full layout flex-1"
+        style={{ height: `calc(100vh - 150px)` }}
+      >
         <Component {...pageProps} />
       </div>
+      <Analytics />
     </div>
   );
 };
